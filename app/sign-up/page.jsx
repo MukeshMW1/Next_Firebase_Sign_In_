4217a9a810fa3e46522e44fa756db6e85b0666bf
Mukesh <mukeshbhattampb@gmail.com>
2024-06-30 13:36:04 +0545

    first commit

---
+++ app/sign-up/page.jsx
@@ -0,0 +1,87 @@
+// SignUp.jsx
+'use client'
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@/app/firebase/config'
+
+const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+
+
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, password);
+            setEmail('');
+            setPassword('');
+            setLoading(false);
+            console.log(user);
+            alert('User signed in successfully');
+        } catch (err) {
+            alert("An unknown error occured");
+            ; console.log(err);
+
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+            <div className="max-w-md w-full space-y-8 p-10 bg-gray-800 rounded-xl shadow-md">
+                <div>
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-100">
+                        Sign Up
+                    </h2>
+                </div>
+                <form className="mt-8 space-y-6" onSubmit={handleSignUp}>
+                    <div className="rounded-md shadow-sm -space-y-px">
+                        <div>
+                            <label htmlFor="email-address" className="sr-only">Email address</label>
+                            <input
+                                id="email-address"
+                                name="email"
+                                type="email"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                placeholder="Email address"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="sr-only">Password</label>
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                placeholder="Password"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        {error && <p className="text-red-500 text-xs italic">{error}</p>}
+                    </div>
+                    <div>
+                        <button
+                            type="submit"
+                            disabled={loading}
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            {loading ? 'Signing up...' : 'Sign Up'}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
